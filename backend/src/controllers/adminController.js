1577b890a903aa3ab58ebfa855ccccedf3d6b229
shivam <shivamrawat1053@gmail.com>
2026-06-10 21:06:25 +0530

    case fix

--- backend/src/controllers/adminController.js
+++ backend/src/controllers/adminController.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const User = require("../models/user");
 const Attendance = require("../models/attendence"); 
 const bcrypt = require("bcryptjs");
 
--- backend/src/controllers/managerController.js
+++ backend/src/controllers/managerController.js
@@ -1,5 +1,5 @@
 const Attendance = require("../models/attendence");
-const User = require("../models/User");
+const User = require("../models/user");
 
 
 exports.getTeamAttendance = async (req, res) => {
